b81b10e0e7f2037e53d5aa5c71848e724bd2a8d7
larsnoches <cyr.selyanin@gmail.com>
2022-02-18 23:48:50 +0500

    ticket service init

--- src/main/java/com/cyrilselyanin/bustvm/repository/TicketRepository.java
+++ src/main/java/com/cyrilselyanin/bustvm/repository/TicketRepository.java
@@ -2,6 +2,8 @@ package com.cyrilselyanin.bustvm.repository;
 
 import com.cyrilselyanin.bustvm.domain.Ticket;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource(exported = false)
 public interface TicketRepository extends JpaRepository<Ticket, Long> {
 }
---
+++ src/main/java/com/cyrilselyanin/bustvm/service/TicketService.java
@@ -0,0 +1,15 @@
+package com.cyrilselyanin.bustvm.service;
+
+import com.cyrilselyanin.bustvm.repository.TicketRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class TicketService {
+    private final TicketRepository ticketRepository;
+
+    public TicketService(TicketRepository ticketRepository) {
+        this.ticketRepository = ticketRepository;
+    }
+
+
+}
