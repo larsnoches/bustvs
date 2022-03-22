4c673c4baf4edd22548a3ab99959c20d0ec8fc20
larsnoches <cyr.selyanin@gmail.com>
2022-03-23 02:42:53 +0500

    tickets, added cors method PATCH.

--- src/main/java/com/cyrilselyanin/bustvm/config/WebSecurityConfig.java
+++ src/main/java/com/cyrilselyanin/bustvm/config/WebSecurityConfig.java
@@ -10,6 +10,9 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.authority.mapping.SimpleAuthorityMapper;
 import org.springframework.security.web.authentication.session.NullAuthenticatedSessionStrategy;
 import org.springframework.security.web.authentication.session.SessionAuthenticationStrategy;
+import org.springframework.web.cors.CorsConfiguration;
+
+import java.util.List;
 
 @KeycloakConfiguration
 @EnableGlobalMethodSecurity(prePostEnabled = true)
@@ -31,6 +34,28 @@ public class WebSecurityConfig extends KeycloakWebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         super.configure(http);
         http
+                .cors().configurationSource(request -> {
+                    var corsConfig = new CorsConfiguration();
+                    corsConfig.setAllowedOrigins(
+                            List.of(
+                                    "http://localhost:4200",
+                                    "http://192.168.56.1:4200",
+                                    "http://192.168.56.104:8180"
+                            )
+                    );
+                    corsConfig.setAllowedMethods(
+                            List.of(
+                                    "GET",
+                                    "POST",
+                                    "PUT",
+                                    "PATCH",
+                                    "DELETE",
+                                    "OPTIONS"
+                            )
+                    );
+                    corsConfig.setAllowedHeaders(List.of("*"));
+                    return corsConfig;
+                }).and()
                 .csrf().disable()
                 .authorizeRequests()
                 .anyRequest().fullyAuthenticated();
